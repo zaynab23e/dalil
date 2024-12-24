1dc9c110ef7a188b2c7dfd32feeaf69341ee7735
zaynab10o <zaynabm766@gmail.com>
2024-12-24 13:16:40 +0200

    in

--- app/Http/Controllers/User/AuthController.php
+++ app/Http/Controllers/User/AuthController.php
@@ -3,57 +3,63 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-use App\Http\Requests\user\createUser;
-use App\Http\Requests\user\loginUser;
-use App\Http\Requests\user\ForgotRequest;
-use App\Http\Requests\user\ResetPass;
+use App\Http\Requests\user\forgot;
+use App\Http\Requests\user\reset;
+use App\Http\Requests\user\login;
+use App\Http\Requests\user\store;
 use App\Models\User;
-use resetPassword;
-use Hash;
-use Auth;
-use Mail;
-use DB;
+use App\Notifications\resetPassword;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
 class AuthController extends Controller
 {
-    public function register(createUser $request)
-        {
-        $validatedData=$request->validated();
-        $user=User::create([
-            'name'=>$validatedData['name'],
-            'email'=>$validatedData['email'],
-            'password'=>Hash::make($validatedData['password']),
-        ]);
-        return response()->json(['msg'=>'successfully']);
-        }
-        
-        public function login( loginUser $request){
-            $user = User::where('email', $request->input('email'))->first();
-            if (! $user ) {
-                return response()->json(['msg' => 'error']);
-            }
-            $token = $user->createToken('api of token', [$user->name])->plainTextToken;
-            return response()->json(['token' => $token]);
-        }
-        public function logout(){
-            $user= Auth::user();
-            
-        Auth::logout();
-            return response()->json(['msg'=>'deleted successfully']);
-        }
+    public function register(store $request)
+    {
+        $validatedData = $request->validated();
+        $validatedData['password'] = Hash::make($request->password);
+        $user = User::create($validatedData);
+        return response()->json('تم التسجيل بنجاح', 201);
+    }
 
-        public function forgotPassword(ForgotRequest  $request){
-            $validatedData=$request->validated();
-        
+    public function login(login $request)
+    {
+        $user = User::where('email', $request->input('email'))->first();
+        if (!$user) {
+            return response()->json('المستخدم غير موجود', 404);
+        }
+        $token = $user->createToken('api of token', [$user->name])->plainTextToken;
+        return response()->json(
+            [
+                'user' => $user,
+                'token' => $token
+            ]
+        );
+    }
+    public function logout()
+    {
+
+        $user = Auth::user();
+        Auth::user()->tokens()->delete();
+        return response()->json('تم تسجيل الخروج بنجاح', 200);
+    }
+
+
+
+    public function forgotPassword(forgot $request)
+    {
+
+        $validatedData = $request->validated();
         $isEmail = filter_var($request->identifier, FILTER_VALIDATE_EMAIL);
         $user = $isEmail
             ? User::where('email', $request->identifier)->first()
             : User::where('phone', $request->identifier)->first();
-        
+
         if (!$user) {
-            return response()->json(['message' => 'المستخدم غير موجود'], 404);
+            return response()->json('المستخدم غير موجود', 404);
         }
-        
+
         $code = mt_rand(1000, 9999);
         $code = str_pad($code, 4, '0', STR_PAD_LEFT);
         DB::table('password_reset_tokens')->updateOrInsert(
@@ -63,7 +69,7 @@ class AuthController extends Controller
                 'created_at' => now()
             ]
         );
-        
+
         if ($isEmail) {
             Mail::raw("رمز إعادة تعيين كلمة المرور الخاص بك هو: $code", function ($message) use ($user) {
                 $message->to($user->email)
@@ -73,56 +79,34 @@ class AuthController extends Controller
         } else {
             $user->notify(new resetPassword($code));
             return response()->json(['message' => 'تم إرسال رمز إعادة تعيين كلمة المرور إلى هاتفك']);
-        }  
+        }
+    }
+
+
+    public function resetPassword(reset $request)
+    {
+
+        $isEmail = filter_var($request->identifier, FILTER_VALIDATE_EMAIL);
+        $resetEntry = DB::table('password_reset_tokens')
+            ->where($isEmail ? 'email' : 'email', $request->identifier)
+            ->where('token', $request->code)
+            ->first();
+
+        if (!$resetEntry) {
+            return response()->json([
+                'message' => 'رمز غير صالح',
+            ], 404);
         }
 
+        $user = $isEmail
+            ? User::where('email', $request->identifier)->first()
+            : User::where('phone', $request->identifier)->first();
 
-        public function resetPassword( ResetPass  $request){
+        $user->password = Hash::make($request->password);
+        $user->save();
 
-            $isEmail = filter_var($request->identifier, FILTER_VALIDATE_EMAIL);
-            $resetEntry = DB::table('password_reset_tokens')
-                ->where($isEmail ? 'email' : 'email', $request->identifier)
-                ->where('token', $request->code)
-                ->first();
-            
-            if (!$resetEntry) {
-                return response()->json([
-                    'message' => 'رمز غير صالح',
-                ], 404);
-            }
-            
-            $user = $isEmail
-                ? User::where('email', $request->identifier)->first()
-                : User::where('phone', $request->identifier)->first();
-            
-            $user->password = Hash::make($request->password);
-            $user->save();
-            
-            DB::table('password_reset_tokens')->where($isEmail ? 'email' : 'email', $request->identifier)->delete();
-            return response()->json(['message' => 'تم إعادة تعيين كلمة المرور بنجاح']);
-        }
-        
-
-
-
-
-        // public function verifyEmail(Request $request){
-        //     $request->validate([
-        //         'email' => 'required|email',
-        //         'verification_code' => 'required|numeric',
-        //     ]);
-        //     $user = User::where('email', $request->email)
-        //                 ->where('verification_code', $request->verification_code)
-        //                 ->first();
-        //     if (!$user) {
-        //         return response()->json( 'Invalidcode or email');
-        //     }
-        //     $user->verified_at = now();
-        //     $user->verification_code = null;
-        //     $user->save();
-        //     return response()->json(['message' => 'Email verified successfully']);    
-        // }
-
+        DB::table('password_reset_tokens')->where($isEmail ? 'email' : 'email', $request->identifier)->delete();
+        return response()->json(['message' => 'تم إعادة تعيين كلمة المرور بنجاح']);
+    }
 
 }
-
