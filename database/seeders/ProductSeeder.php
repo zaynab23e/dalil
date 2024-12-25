9eb46ff09971860d16c2e21913792cdef315558d
zaynab10o <zaynabm766@gmail.com>
2024-12-25 15:56:39 +0200

    u

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ProductSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('products')->insert([
+            // Insert your data here
+            'name' => 'Sample Product',
+            'price' => 100,
+        ]);
+    }
+    
+}
