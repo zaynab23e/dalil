3f2b8594ea299bd15edadc63cd6a8b5a550281ab
zaynab10o <zaynabm766@gmail.com>
2024-12-26 12:46:55 +0200

    final merge

--- app/Http/Controllers/Admin/PlaceController.php
+++ app/Http/Controllers/Admin/PlaceController.php
@@ -3,9 +3,89 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\place\store;
+use App\Http\Requests\place\update;
+use App\Models\Category;
+use App\Models\Place;
 
 class PlaceController extends Controller
 {
 
+    public function index()
+    {
+       $places = Place::with('category')->Paginate(15);
+        return view('place.index', compact('places'));
+    }
+
+public function show($id)
+{
+    $place = Place::with('category')->find($id);
+    return view('place.show', compact('place'));
+}
+
+public function create()
+{
+    $categories = Category::all();
+    return view('place.create', compact('categories'));
+}
+
+public function store(store $request)
+{
+    $validatedData = $request->validated();
+    $place =Place::create($request->except('images'));
+
+    if ($request->has('images')) {
+        foreach ($request->file('images') as $image) {
+            try {
+                $imagePath = $image->store('places', 'public');
+                $place->images()->create([
+                    'image' => $imagePath,
+                ]);
+            } catch (\Exception $e) {
+                return redirect()->back()->withErrors(['error' => $e->getMessage()]);
+            }
+        }
+    }
+    return redirect()->route('admin.places.index')->with('success', 'Place created successfully');
+}
+
+public function edit(Place $place, $id)
+{
+    $place = Place::with('images','category')->find($id);
+    $categories = Category::all();
+    return view('place.edit', compact('place', 'categories'));
+}
+
+public function update(update $request, $id)
+{
+    $place = Place::find($id);
+    $validatedData = $request->validated();
+    $place->update($request->except('images'));
+
+    if ($request->has('images')) {
+        foreach ($request->file('images') as $image) {
+            try {
+                $imagePath = $image->store('places', 'public');
+                $place->images()->create([
+                    'image' => $imagePath,
+                ]);
+            } catch (\Exception $e) {
+                return redirect()->back()->withErrors(['error' => $e->getMessage()]);
+            }
+        }
+    }
+    return redirect()->route('admin.places.index')->with('success', 'Place updated successfully');
+}
+
+public function destroy($id)
+{
+    $place = Place::find($id);
+    $place->delete();
+    return redirect()->route('admin.places.index')->with('success', 'Place deleted successfully');
+}
+
+public function test()
+{
+    return view('test');
+}
 }
